4286fda33bcec7816fb9bdc8ba31d3df27e5e5e4
alpeshrathod <apr.alpesh@gmail.com>
2020-11-09 15:46:27 +0530

    Added Scaffold to Materail App

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First App'),
+        ),
+        body: Text('This is my default app!'),
+      ),
+    );
   }
-
 }
