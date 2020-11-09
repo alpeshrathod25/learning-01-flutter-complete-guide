f99210c3cebfeda7fd79cc66ac0a24d802e0645c
alpeshrathod <apr.alpesh@gmail.com>
2020-11-09 16:32:16 +0530

    Added anonymous function

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  
+  void answerQuestion(){
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+  var questions = [
+    "What\'s your favourite color?",
+    "What\'s your favourite aniamrl?",
+  ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('First App'),
         ),
-        body: Text('This is my default app!'),
+        body: Column(
+          children: [
+            Text('This is question'),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: ()=>print('Answer two chosen with INLINE FUNCTION'),
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: (){
+                print('Answer three chosen with ANONYMOUS FUNCTION');
+              },
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
